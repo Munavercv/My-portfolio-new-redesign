d9b8190604a283ae4b80b7124f9f25333e58f90e
Munavercv <munavermunu80@gmail.com>
2024-07-27 11:04:09 +0530

    improved website perfomance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,71 @@
+/* typig-text */
+	const typedTextSpan = document.querySelector(".typed-text");
+	const cursorSpan = document.querySelector(".cursor");
+
+	const textArray = ["Web Designer", "Graphic Designer", "Freelancer"];
+	const typingDelay = 200;
+	const erasingDelay = 10;
+	const newTextDelay = 100; // Delay between current and next text
+	let textArrayIndex = 0;
+	let charIndex = 0;
+
+	function type() {
+		if (charIndex < textArray[textArrayIndex].length) {
+			if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+			typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+			charIndex++;
+			setTimeout(type, typingDelay);
+		} else {
+			cursorSpan.classList.remove("typing");
+			setTimeout(erase, newTextDelay);
+		}
+	}
+
+	function erase() {
+		if (charIndex > 0) {
+			// add class 'typing' if there's none
+			if (!cursorSpan.classList.contains("typing")) {
+				cursorSpan.classList.add("typing");
+			}
+			typedTextSpan.textContent = textArray[textArrayIndex].substring(0, 0);
+			charIndex--;
+			setTimeout(erase, erasingDelay);
+		} else {
+			cursorSpan.classList.remove("typing");
+			textArrayIndex++;
+			if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+			setTimeout(type, typingDelay);
+		}
+	}
+
+	document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+		if (textArray.length) setTimeout(type, newTextDelay + 250);
+	});
+/* typig-text */
+
+/* MOVE TOP */
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("movetop").style.display = "block";
+            } else {
+                document.getElementById("movetop").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+/* MOVE TOP */
+
+
+
 jQuery.noConflict();
 jQuery(document).ready(function($){
 								
